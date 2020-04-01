3f0d7eff69d24e4f89c2ecae680be0b57cf38e32
LibraZYJ <1836686674@qq.com>
2020-04-01 23:56:51 +0800

    Day8

---
+++ Day8/列表的定义和遍历.py
@@ -0,0 +1,26 @@
+"""
+列表的定义和遍历
+Date:2020.4.1
+Author:Yujie_zhao
+"""
+
+list1 = [1, 3, 5, 7, 66]
+print(list1) 
+list2 = ['hello'] * 3
+print(list2) 
+print(len(list1)) 
+print(list1[0])
+print(list1[4])
+print(list1[-1])
+print(list1[-3])
+list1[2] = 300
+print(list1)
+# 通过循环用下标遍历
+for index in range(len(list1)):
+    print(list1[index])
+# 通过for循环遍历
+for elem in list1:
+    print(elem)
+# 通过enumerate函数处理列表之后再遍历可以同时获得元素索引和值
+for index, elem in enumerate(list1):
+    print(index, elem)
