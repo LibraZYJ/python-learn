bd1b47a50ddbece2b57e3a5502c6d87a8d649e0d
LibraZYJ <1836686674@qq.com>
2020-04-05 23:27:00 +0800

    爬虫

--- 基本语法/Day11/新闻数据接口.py
+++ 基本语法/Day11/新闻数据接口.py
@@ -3,7 +3,7 @@ requests请求api，用字典和列表保存为json
 @Date 2020.4.4
 Author：Yujie_zhao
 """
-import json
+import json·
 import requests
 resp = requests.get(
     'http://api.tianapi.com/allnews/index?key=28873d5ac82af9c4a6357f0d6b4f59ce&num=10&col=7'
