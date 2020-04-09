3979a8bc21daa2b1dbe741b5a55c3fb7b81d50aa
LibraZYJ <1836686674@qq.com>
2020-04-09 23:54:22 +0800

    Day16

--- 基本语法/Day15/绘制表盘.py
+++ 基本语法/Day15/绘制表盘.py
@@ -9,5 +9,5 @@ from pyecharts import charts
 gauge = charts.Gauge()
 gauge.add('Python小例子', [('Python机器学习', 30),
                         ('Python基础', 70.), ('Python正则', 90)])
-gauge.render(path="仪表盘.html")
+gauge.render(path="./res/仪表盘.html")
 print('ok')
